482f807f954fff02fac754caa6e3c4b36220b19d
Kong Chae Won <chaewonkong@gmail.com>
2021-09-12 18:09:20 +0900

    feat: 🎸 create getAllMarkets controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
 import { AppService } from './app.service';
+import { Market } from './interfaces/market.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('markets')
+  getAllMarkets(): Observable<Market[]> {
+    return this.appService.getAllMarkets().pipe(map((res) => res.data));
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
+import { Market } from './interfaces/market.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  getAllMarkets(): Observable<AxiosResponse<Market[]>> {
+    // Get all market data data from UPbit
+    return this.httpService.get('https://api.upbit.com/v1/market/all');
   }
 }
